806b5a64a102b3e3a28afea8310540204ae20b9e
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-11 18:02:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '𝐉𝐚𝐫𝐨𝐭_𝐨𝐟𝐟𝐜'
-  var sticker_author = '𝐉𝐚𝐫𝐨𝐭'
+  var sticker_name = '𝐑𝐢𝐬𝐦𝐚_𝐨𝐟𝐟𝐜'
+  var sticker_author = '𝐑𝐢𝐬𝐦𝐚'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© 𝐉𝐚𝐫𝐨𝐭'
+global.wm = '© 𝐑𝐢𝐬𝐦𝐚'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
